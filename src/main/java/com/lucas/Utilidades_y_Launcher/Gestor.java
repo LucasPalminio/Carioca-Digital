cfcd2dceec80e32b1339afa1db112b44e98c1c5d
LORENZO <l.devia02@ufromail.cl>
2020-08-23 17:09:22 -0400

    Se creó la clase Gestor

---
+++ src/main/java/com/lucas/Utilidades_y_Launcher/Gestor.java
@@ -0,0 +1,10 @@
+package com.lucas.Utilidades_y_Launcher;
+
+import java.io.File;
+
+public class Gestor {
+    private void crearArchivo(){
+    }
+    private static void eliminarArchivo(){
+    }
+}
