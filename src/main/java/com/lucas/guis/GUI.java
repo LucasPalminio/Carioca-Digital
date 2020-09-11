27b39b6f65e5b086c002f89d153551dfe296eaf3
LORENZO <l.devia02@ufromail.cl>
2020-09-11 17:55:59 -0300

    Se creó la estructura conceptual deseada para la gui.

---
+++ src/main/java/com/lucas/guis/GUI.java
@@ -0,0 +1,14 @@
+package com.lucas.guis;
+
+import javax.swing.*;
+
+public class GUI {
+    private JTable jugadores;
+    private JList triosEscalas;
+    private JButton mazo;
+    private JButton cartaEnLaMesa;
+    private JLabel rondaActual;
+    private JLabel objetivoTrios;
+    private JLabel objetivoEscalas;
+    private JToolBar menuCartas;
+}
