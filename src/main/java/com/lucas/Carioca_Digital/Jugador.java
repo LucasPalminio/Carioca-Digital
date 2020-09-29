d00310d0309b60fcaaa7fd17b4fd34866031e4f3
Fernando Robles <f.robles03@ufromail.cl>
2020-09-29 19:28:40 -0300

    Se agrego nueva documentacion en el package "com.luca.Carioca_Digital" y en el package "com.luca.Utilidades_y_Launcher". Aun falta agregar y editar algunas clases

--- src/main/java/com/lucas/Carioca_Digital/Jugador.java
+++ src/main/java/com/lucas/Carioca_Digital/Jugador.java
@@ -9,6 +9,22 @@ import java.util.Collections;
 import java.util.Scanner;
 
 public class Jugador {
+    /**
+     * Esta clase se encarga de las cartas que posee el jugador, tanto como para asignar el puntaje al final de
+     * cada ronda, como verificar si los trios y escalas son coerentes
+     *  @param nombre
+     * @param cartas
+     * @param puntaje
+     * @param puntajeRonda
+     * @param bajoSuCarta
+     * @param yaSacoCarta
+     * @param matrizTrios
+     * @param matrizEscalas
+     * @param NROTRIOS_a_formar
+     * @param NROESCALAS_a_formar
+     * @param in
+     *
+     */
     private String nombre;
     private ArrayList<Carta> cartas = new ArrayList<>();
     private int puntaje;
@@ -20,24 +36,42 @@ public class Jugador {
 
     public static Scanner in = new Scanner(System.in);
 
+    /**
+     * Constructor del jugador, indicando nombre puntaje y el estado de sus cartas
+     *
+     * @param nombre nombre del jugador
+     */
     public Jugador(String nombre) {
         this.nombre = nombre;
         this.puntaje = 0;
         this.bajoSusCarta = false;
         this.yaSacoCarta = false;
     }
+    /**
+     * @return devuelve el nombre del jugador
+     */
 
     public String getNombre() {
         return nombre;
     }
+    /**
+     * @return devuelve el puntaje del jugador
+     */
+
     public int getPuntaje() {
         return puntaje;
     }
+    /**
+     * @return devuelve el puntaje de la ronda
+     */
 
     public int getPuntajeRonda() {
         return puntajeRonda;
     }
 
+    /**
+     * este metodo calcula el puntaje de la mano del jugador total
+     */
     public void calcularPuntaje(){
         if(cartas.size()>0) {
             for (Carta carta : cartas) {
@@ -45,6 +79,10 @@ public class Jugador {
             }
         }
     }
+
+    /**
+     * este metodo calcula el puntaje de la mano del jugador en cada ronda
+     */
     public void calcularPuntajeRonda(){
         if(cartas.size()>0) {
             for (Carta carta : cartas) {
@@ -53,44 +91,90 @@ public class Jugador {
         }
     }
 
+    /**
+     * @return retorna las cartas que obtiene el jugador
+     */
     public ArrayList<Carta> getCartas() {
         return cartas;
     }
 
+    /**
+     * se instancia la variable PuntajeRonda en la clase actual
+     *
+     * @param puntajeRonda puntaje obtenido en la ronda actual
+     */
     public void setPuntajeRonda(int puntajeRonda) {
         this.puntajeRonda = puntajeRonda;
     }
 
+    /**
+     * se instancia el arreglo de cartas en la clase actual
+     *
+     * @param cartas son las cartas de cada jugador
+     */
     public void setCartas(ArrayList<Carta> cartas) {
         this.cartas = cartas;
     }
+
+    /**
+     * es la carta que agregara el jugador a su mano
+     *
+     * @param carta es la carta que se encuentra en la mesa
+     */
     protected void agregarCarta(Carta carta){
         cartas.add(carta);
     }
+
+    /**
+     * @return devuelne el valor de la carta
+     */
     public int getNroCartas(){ return cartas.size(); }
 
+    /**
+     * @return????
+     */
     public boolean isBajoSusCarta() {
         return bajoSusCarta;
     }
+
+    /**
+     * se instancia ?? en la clase actual
+     *
+     * @param bajoSusCarta
+     */
     public void setBajoSusCarta(boolean bajoSusCarta) {
         this.bajoSusCarta = bajoSusCarta;
     }
 
+    /**
+     * @return luego de sacar una carta se le habilita la opcion de botar carta en el pozo
+     */
     public boolean isYaSacoCarta() {
         return yaSacoCarta;
     }
 
+    /**
+     * se instancia yaSacoCarta en la clase actual
+     *
+     * @param yaSacoCarta
+     */
     public void setYaSacoCarta(boolean yaSacoCarta) {
         this.yaSacoCarta = yaSacoCarta;
     }
 
+    /**
+     * Este metodo funciona solamente cuando finaliza la ronda,su funcion es limpia las matrices
+     * de trios y escalas que el jugador tenga en la mesa
+     */
     protected void limpiarMatriz(){
-        //Este metodo funciona solamente cuando finaliza la ronda,
-        // su funcion es limpia las matrices de trios y escalas que el jugador tenga en la mesa
         matrizTrios.clear();
         matrizEscalas.clear();
     }
 
+    /**
+     * @param indice posicion de la carta en la mano del jugador
+     * @return Este metodo se encarga de guardar la carta que selecciono el jugador para botarla en el pozo
+     */
     public Carta botarCarta(int indice){
         Carta cartaABotar = cartas.get(indice);
         cartas.remove(indice);
--- src/main/java/com/lucas/Utilidades_y_Launcher/Launcher.java
+++ src/main/java/com/lucas/Utilidades_y_Launcher/Launcher.java
@@ -3,8 +3,13 @@ package com.lucas.Utilidades_y_Launcher;
 import com.lucas.guis.MenuPrincipalGUI;
 
 public class Launcher {
+    /**
+     * Lanzador del programa, haciendo visible la ventana de la interfaz grafica
+     * @param args son los argumentos que recibe el metodo
+     */
 
     public static void main(String[] args) {
+
 /*
         Utilidades.setIn(new Scanner(System.in).useDelimiter("\n"));
         MenuPrincipal.menuPrincipal();
